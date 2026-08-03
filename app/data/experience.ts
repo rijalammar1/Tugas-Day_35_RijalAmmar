fa32ff77b13b354754302633b8e84124ddf4add2
rijalammar1 <rijaltn13@gmail.com>
2026-08-03 09:57:20 +0700

    ui: improve experience section

--- app/data/experience.ts
+++ app/data/experience.ts
@@ -4,9 +4,9 @@ export const experiences = [
     period: "Aug 2023 - Dec 2023",
     company: "ADS Digital Partner",
     description: [
-      "Understood REST API and JSON concepts to support frontend-backend integration.",
-      "Collaborated with Backend Engineers to integrate REST APIs into web and mobile applications.",
-      "Developed responsive user interfaces using Laravel, Flutter, Next.js, and Tailwind CSS.",
+      "Contributed approximately 50% to understanding and implementing REST APIs and JSON within the project.",
+      "Contributed approximately 50% to integrating REST APIs in collaboration with Backend Engineers.",
+      "Completed 100% of assigned front-end development tasks by implementing UI designs using Laravel, Flutter, Next.js, and Tailwind CSS.",
     ],
   },
   {
@@ -14,9 +14,9 @@ export const experiences = [
     period: "Dec 2018 - Nov 2019",
     company: "PT. Universal Big Data",
     description: [
-      "Designed and developed the Enotaris web application using PHP, HTML, CodeIgniter, AJAX, and SQL Database.",
-      "Installed and configured the Enotaris application for clients while providing remote technical support using TeamViewer.",
-      "Created tutorial videos with Camtasia to help clients onboard and use the Enotaris application effectively.",
+      " Designed and implemented an Enotaris website using PHP, HTML, CI, AJAX, SQL Database, contributed 100% to the implementation of web developer tasks",
+      "Installing the Enotaris website for clients and providing follow-up if clients encounter difficulties in running the Enotaris website using TeamViewer software, contributing 100% to providing follow-up to clients.",
+      "Creating tutorial videos on how to run the Enotaris website using Camtasia software, contributing 100% to the production of tutorial videos.",
     ],
   },
 ];
